fc0699bc3b3a2107bb21404946bf83dc8c1e3310
RiccardoS <rspinellir13@gmail.com>
2024-12-05 21:08:17 +0100

    Contraseñas Terminada

--- Contraseña/script-contraseña.js
+++ Contraseña/script-contraseña.js
@@ -2,9 +2,43 @@ const mayu = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "
 const minu = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const sim = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+const boton = document.getElementById('btnContra')
+const resultado = document.getElementById('divResultado')
+const inputUsuario = document.getElementById('inputUsuario')
+
+// --- Aleatoriedad -- //
 
 function getAleat(arr) {
     return Math.floor(Math.random() * arr.length);
 }
 
-  console.log(mayu[getAleat(mayu)])
+// --- Generador de contraseña -- //
+
+const contraseña = (userValue) => {
+  let contra = [];
+  let alphaN = []
+
+  alphaN.push(mayu, minu, num, sim)
+  
+  while (contra.length < userValue) {
+    alphaN.forEach(element => {
+  
+      contra.push(element[getAleat(element)])
+  
+  });
+  }
+  while (contra.length != userValue) {
+    contra.pop()
+  }
+  
+  return contra.join('')
+}
+
+// --- Boton -- //
+
+boton.addEventListener('click', () => {
+  if (inputUsuario.value.trim() !== "") {
+      resultado.innerHTML = `<p class="texto">Tu contraseña es:</p>
+            <p id="pResultado">${contraseña(inputUsuario.value)}</p>`
+  }
+});
