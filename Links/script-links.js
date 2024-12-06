a673ad183300a8c93a2abac3fb9e05064d701743
RiccardoS <rspinellir13@gmail.com>
2024-12-06 21:30:57 +0100

    Listado en Links actualizado

--- Links/script-links.js
+++ Links/script-links.js
@@ -1,1 +1,17 @@
-/// usar gorro link para eliminar los links
+const nomPag = document.getElementById('nombrePagina')
+const linkPag = document.getElementById('linkPagina')
+const botonAgregar = document.getElementById('agregar')
+const listaLinks = document.getElementById('listaLinks')
+
+const listado = (userInput) => {
+    
+    const newLi = document.createElement("li");
+    const newText = document.createTextNode(userInput);
+
+    newLi.appendChild(newText);
+    listaLinks.appendChild(newLi);
+}
+
+botonAgregar.addEventListener('click', () => {
+    listado(nomPag.value)
+})
