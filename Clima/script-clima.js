d028ea22b66f9d0854786622997916e9ed91d0ad
RiccardoS <rspinellir13@gmail.com>
2024-12-11 00:01:14 +0100

    Clima Terminado

--- Clima/script-clima.js
+++ Clima/script-clima.js
@@ -1,4 +1,16 @@
-const urlApi = 'http://api.weatherapi.com/v1/current.json?key=865f197a87974242bc1181451240912&q=Madrid&aqi=no';
+const urlApi ='http://api.weatherapi.com/v1/forecast.json?key=865f197a87974242bc1181451240912&q=madrid&days=7&aqi=no&alerts=no'
+
+// --- Actual --- //
+const divActual = document.getElementById('actual')
+const city = document.getElementById('location')
+const textActual = document.getElementById('textActual')
+const currentData = document.getElementById('currentData')
+const listaActual = document.getElementById('listaActual')
+
+// --- Forecast --- //
+const forecastList = document.getElementById('forecastList')
+
+// --- Llamada api --- //
 
 const apiCall = async (urlApi) => {
     try {
@@ -17,7 +29,41 @@ const apiCall = async (urlApi) => {
 apiCall(urlApi)
     .then((data) => {
         console.log(data);
+        tiempoActual(data)
+        iterForecast(data)
     })
     .catch((error) => {
         alert('No se ha podido obtener los datos del clima.');
+});
+
+// --- FUNCIONES DOM --- //
+
+const tiempoActual = (data) => {
+
+    city.innerText = `${data.location.region} / ${data.location.country}`
+    textActual.innerText = `${data.current.condition.text}`
+
+    currentData.innerHTML = `
+       <img id="iconActual" src="${data.current.condition.icon}" alt="Icono Condicion Actual">
+       <p id="tempActual">${data.current.temp_c}</p>
+       <img id="celcius" src="/Img/celcius.png" alt="celcius">
+    `
+
+    listaActual.innerHTML = `
+        <li>Presipitaciones: ${data.current.precip_mm} mm</li>
+        <li>Humedad: ${data.current.humidity} %</li>
+        <li>Viento: ${data.current.wind_kph} Km/h</li>
+    ` 
+}
+
+const iterForecast = (data) => {
+    data.forecast.forecastday[0].hour.forEach(element => {
+        forecastList.innerHTML += `
+            <li class="itemForecast">
+            <p>${element.time.slice(-5)}</p>
+            <img src="${element.condition.icon}" alt="Icono Condicion">
+            <p>${element.temp_c} °C</p>
+            </li>
+        `
     });
+}
